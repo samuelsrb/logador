7d319aae36363772e519165da32400f376bd1e3e
samuel soares rocha barroso <samuelbarroso0733@digitalcollege.internal>
2026-04-09 14:22:08 -0300

    create and structure of recovery in javascript

---
+++ src/scripts/recovery.js
@@ -0,0 +1,43 @@
+const username = document.querySelector("#username-recovery");
+const new_password = document.querySelector("#new-password-recovery");
+const confirm_new_password = document.querySelector("#confirm-new-password-recovery");
+const button = document.querySelector("#button-recovery");
+
+button.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    const usernameValue = username.value;
+    const newPasswordValue = new_password.value;
+    const confirmPasswordValue = confirm_new_password.value;
+
+    if (usernameValue === "" || newPasswordValue === "" || confirmPasswordValue === "") {
+        alert("Preencha todos os campos!")
+        return
+    }
+
+    const usernameSession = sessionStorage.getItem('username')
+    console.log(usernameSession)
+
+    if (usernameValue !== usernameSession) {
+        alert("Usuário nâo encontrado!")
+        return
+    }
+
+    if (newPasswordValue !== confirmPasswordValue) {
+        alert("Senhas nâo conferem");
+        return
+    }
+
+    const passwordSession = sessionStorage.getItem('password')
+
+    if (newPasswordValue === passwordSession) {
+        alert("Senha indisponível.");
+        return
+    }
+
+    sessionStorage.setItem('password', newPasswordValue);
+
+    alert("senha redefinida com sucesso.");
+
+    window.location.href = './login.html';
+})
